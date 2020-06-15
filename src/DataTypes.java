778e885bb61a4a5a1c99579841a0d973e5120359
amayacausey <amayacausey@gmail.com>
2020-06-15 13:33:56 -0700

    first commit

---
+++ src/DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static void main(String[] args){
+        boolean isSkyBlue = true;
+        byte b = -128;
+        //Bytes are not commonly used
+        char myChar = 'a';
+        short myShortVariable = -3000;
+        int myInteger = 9000;
+        float myFloat = 7.24f;
+        double myDouble = -90.28333;
+        long longVariable = 9999990;
+
+        System.out.println("My boolean variable : " + isSkyBlue);
+
+    }
+}
